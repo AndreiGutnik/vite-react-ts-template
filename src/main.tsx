8aa5314fa70b80285e54d2021a3e865d48b449be
Andrii Hutnyk <andreigutnik@gmail.com>
2024-03-23 08:43:48 +0100

    Update template

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,24 @@
-import React from "react"
-import { createRoot } from "react-dom/client"
-import { Provider } from "react-redux"
-import App from "./App"
-import { store } from "./app/store"
-import "./index.css"
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
+//import { Provider } from 'react-redux';
+//import { persistor, store } from './redux/store';
+import { BrowserRouter } from 'react-router-dom';
 
-const container = document.getElementById("root")
+import { App } from './components/App';
 
-if (container) {
-  const root = createRoot(container)
+//import { PersistGate } from 'redux-persist/integration/react';
 
-  root.render(
-    <React.StrictMode>
-      <Provider store={store}>
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    {/* <Provider store={store}> */}
+    {/* <PersistGate loading={null} persistor={persistor}> */}
+    <BrowserRouter>
+      <HelmetProvider>
         <App />
-      </Provider>
-    </React.StrictMode>,
-  )
-} else {
-  throw new Error(
-    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
-  )
-}
+      </HelmetProvider>
+    </BrowserRouter>
+    {/* </PersistGate> */}
+    {/* </Provider> */}
+  </React.StrictMode>
+);
